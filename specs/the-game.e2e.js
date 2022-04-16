06906c78d6f292a3af088e43a6e251f448411118
Olga-Kuzm <legions1987@gmail.com>
2022-04-16 18:02:36 +0300

    HW 5 fix

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -4,23 +4,28 @@ describe('The game', function () {
         await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
-        await browser.waitUntil(async () => {            
-            const ballLocation = await $('#ball').getLocation();
-            const padLocation = await $('#pad').getLocation()    
-                while(ballLocation.x > padLocation.x){
-                    await browser.keys('d')
-                    if(ballLocation = padLocation){
-                        break
-                    }
-                };                           
-                while(ballLocation.x < padLocation.x ){
-                    await browser.keys('a')
-                    if(ballLocation = padLocation){
-                        break
-                    }
-                }
+        await browser.waitUntil(async () => { 
+            const ballLocation = await $('#ball').getLocation('x');
+            const padLocation = await $('#pad').getLocation('x')  
             
-
+            while(ballLocation > padLocation){                
+                await browser.keys('d')
+                const ballLocation = await $('#ball').getLocation('x');
+                const padLocation = await $('#pad').getLocation('x')
+                
+                if(ballLocation < padLocation){
+                    break
+                }
+            };
+            while(ballLocation < padLocation){                                
+                await browser.keys('a')
+                const ballLocation = await $('#ball').getLocation('x');
+                const padLocation = await $('#pad').getLocation('x') 
+                
+                if(ballLocation > padLocation){
+                    break
+                }
+            }
             const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
             console.log({ points })
@@ -28,3 +33,4 @@ describe('The game', function () {
         
     });
 })
+
